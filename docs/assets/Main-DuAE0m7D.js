fc3c53e18bc0fcd7c39040b0be578caa52ccd62d
森森 <1851337573@qq.com>
2024-12-24 20:42:53 +0800

    init

---
+++ docs/assets/Main-DuAE0m7D.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{f as s,h as t,o as a}from"./index-4cp2rmX9.js";const c={},n={class:"container"};function r(_,e){return a(),s("div",n,e[0]||(e[0]=[t("span",{class:"welcome"},"WelCome",-1)]))}const i=o(c,[["render",r],["__scopeId","data-v-2e0eeec2"]]);export{i as default};
